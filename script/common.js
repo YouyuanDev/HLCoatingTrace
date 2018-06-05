d7adf164787f5e2bba7fec0de64a9b2c097b31d4
Lfw11 <lfw@youyuantech.com>
2018-06-05 17:20:03 +0800

    TitleBar修改
    TitleBar修改

--- script/common.js
+++ script/common.js
@@ -1,9 +1,10 @@
 var header, headerHeight = 0;
-var serverIP='192.168.0.14:8080';
+var serverIP='192.168.0.12:8080';
+
 function fnReadyHeader() {
     header = $api.byId('header');
     if (header) {
-        //$api.fixIos7Bar(header);
+        $api.fixIos7Bar(header);
         headerHeight = $api.offset(header).h;
     }
 };
@@ -30,4 +31,28 @@ function JudgeLogin(){
    }else{
       return false;
    }
+}
+function formatterdate(value,row,index){
+    //alert(toString.call(value));
+    if(value!=undefined&&value!=null)
+        return getDateOnly(value);
+    else
+        return "";
+}
+function getDateOnly(str){
+    var oDate = new Date(str);
+    y=oDate.getFullYear();
+    m = oDate.getMonth()+1;
+    d = oDate.getDate();
+    return  y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d);
+}
+function getDate1(str){
+    var oDate = new Date(str);
+    y=oDate.getFullYear();
+    m = oDate.getMonth()+1;
+    d = oDate.getDate();
+    h=oDate.getHours();
+    mins=oDate.getMinutes();
+    s=oDate.getSeconds();
+    return  y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d)+' '+(h<10?('0'+h):h)+':'+(mins<10?('0'+mins):mins)+':'+(s<10?('0'+s):s);
 }
